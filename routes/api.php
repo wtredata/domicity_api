a604924cbd09977048c31ec0c86702c00306a8f7
LuisOlaya <luisolaya1997@hotmail.com>
2021-05-08 19:01:51 -0500

    departmentCity

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::resource('city', 'CityController');
 Route::resource('department', 'DepartmentController');
-Route::get('stateCity/{department}', 'CityController@index');
+Route::get('stateCity/{department}', 'DepartmentCityController@index');
 Route::resource('solicitude', 'SolicitudeController');
 
